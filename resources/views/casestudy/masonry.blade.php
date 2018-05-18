1b3739ff5f23a56a7a3e7a32115a5c6a2e0b61b7
Luke Warner <lukeswarner@gmail.com>
2018-05-18 15:55:35 -0400

    corrected filtering to use bootstrap dropdown

--- resources/views/casestudy/masonry.blade.php
+++ resources/views/casestudy/masonry.blade.php
@@ -1,5 +1,6 @@
 @extends('library-app')
 
+
 @section('scripts')
   @include('scripts.masonry')
 @endsection
@@ -12,80 +13,95 @@
 <div class="container-fluid container-wide">
 
 
-  <div class="common-bar">
+  <div class="welcome-bar">
     <div class="row">
-      <span class="lead-text">Welcome</span>
-Contràriament a la creença popular, Lorem Ipsum no és només text aleatori. Té les seves arrels en una peça clàssica de la literatura llatina del 45 aC, és a dir, de fa 2000 anys. Richard McClintock, un professor de llatí al Hampden-Sydney College a Virgínia, va buscar una de les paraules més estranyes del llatí, "consectetur", procedent d'un dels paràgrafs de Lorem Ipsum, i anant de citació en citació d'aquesta paraula a la literatura clàssica, en va descobrir l'orígen veritable. Lorem ipsum procedeix de les seccions 1.10.32 i 1.10.33 de "De Finibus Bonorum et Malorum" (Sobre el Bé i el Mal) de Ciceró, escrit l'any 45 aC. Aquest llibre és un tractat sobre la teoria de l'ètica, molt popular durant el Renaixement. La primera línia de Lorem Ipsum, "Lorem ipsum dolor sit amet..", prové d'una línia a la secció 1.10.32.
+      <div class="col-xs-12 col-lg-3">
+        <span class="lead-text">Welcome</span>
+      </div>
+      <div class="col-xs-12 col-lg-9">
+        Contràriament a la creença popular, Lorem Ipsum no és només text aleatori. Té les seves arrels en una peça clàssica de la literatura llatina del 45 aC, és a dir, de fa 2000 anys. Richard McClintock, un professor de llatí al Hampden-Sydney College a Virgínia, va buscar una de les paraules més estranyes del llatí, "consectetur", procedent d'un dels paràgrafs de Lorem Ipsum, i anant de citació en citació d'aquesta paraula a la literatura clàssica, en va descobrir l'orígen veritable. Lorem ipsum procedeix de les seccions 1.10.32 i 1.10.33 de "De Finibus Bonorum et Malorum" (Sobre el Bé i el Mal) de Ciceró, escrit l'any 45 aC. Aquest llibre és un tractat sobre la teoria de l'ètica, molt popular durant el Renaixement. La primera línia de Lorem Ipsum, "Lorem ipsum dolor sit amet..", prové d'una línia a la secció 1.10.32.
 
-i vols fer servir un passatge de Lorem Ipsum, has d'estar segur que no hi haurà res comprometedor amagat en el text. Tots els generadors de Lorem ipsum a Internet tendeixen a repetir un tros determinat tantes vegades com calgui, i això fa que aquest sigui el primer generador real a Internet.
+        i vols fer servir un passatge de Lorem Ipsum, has d'estar segur que no hi haurà res comprometedor amagat en el text. Tots els generadors de Lorem ipsum a Internet tendeixen a repetir un tros determinat tantes vegades com calgui, i això fa que aquest sigui el primer generador real a Internet.
+      </div>
     </div>
   </div> <!-- end common bar -->
 
-  <nav class="navbar navbar-default navbar-centered" id="progress-bar">
+  <nav class="navbar navbar-default navbar-centered" id="filter-bar">
 
-    <div class="collapse navbar-collapse" >
+
+    <div class="" id="app-filterbar-collapse">
 
       <ul class="nav navbar-nav">
-      <div class="btn-group">
 
-        <select class="filters-select">
-          <option value="">Countries</option>
-          @foreach($countries as $country)
-          <option value=".{{ strtolower($country) }}">{{$country}}</option>
-          @endforeach
-        </select>
+        <div class="btn-group">
+          <div class="dropdown">
+            <button id="dropdown-country" type="button" class="btn btn-urc-secondary dropdown-toggle filters-dropdown" data-value="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              All Countries <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu">
+              <li><a class="do_filter" data-filter="country" data-name="All Countries" data-value="" href="#">All Countries</a></li>
+              <li><a class="do_filter" data-filter="country" data-name="Bangladesh" data-value=".bangladesh"  href="#">Bangladesh</a></li>
+              <li><a class="do_filter" data-filter="country" data-name="Paraguay" data-value=".paraguay" href="#">Paraguay</a></li>
+              <li><a class="do_filter" data-filter="country" data-name="Tanzania" data-value=".tanzania" href="#">Tanzania</a></li>
+              <li><a class="do_filter" data-filter="country" data-name="Burundi" data-value=".burundi" href="#">Burundi</a></li>
+            </ul>
+          </div>
+        </div>
 
-        <select class="filters-select">
-          <option value="">Keywords</option>
-          @foreach($keywords as $a)
-          <option value=".k{{ $a->id }}">{{$a->keyword}}</option>
-          @endforeach
-        </select>
+        <div class="btn-group">
+          <button id="dropdown-keyword" type="button" class="btn btn-urc-secondary dropdown-toggle filters-dropdown" data-value="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            All Keywords <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="do_filter" data-filter="keyword" data-name="All Keywords" data-value=""  href="#">All Keywords</a></li>
+            @foreach($keywords as $a)
+              <li><a class="do_filter" data-filter="keyword" data-name="{{$a->keyword}}" data-value=".k{{$a->id}}" href="#">{{$a->keyword}}</a></li>
+            @endforeach
+          </ul>
+        </div>
 
-        <select class="filters-select">
-          <option value="">Methods</option>
-          @foreach($methods as $a)
-          <option value=".m{{ $a->id }}">{{$a->name}}</option>
-          @endforeach
-        </select>
-
-        <select class="filters-select">
-          <option value="">Thematic Areas</option>
-          @foreach($thematics as $a)
-          <option value=".t{{ $a->id }}">{{$a->name}}</option>
-          @endforeach
-        </select>
+        <div class="btn-group">
+          <button id="dropdown-method" type="button" class="btn btn-urc-secondary dropdown-toggle filters-dropdown" data-value="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            All Methods <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="do_filter" data-filter="method" data-name="All Methods" data-value=""  href="#">All Methods</a></li>
+            @foreach($methods as $a)
+              <li><a class="do_filter" data-filter="method" data-name="{{$a->name}}" data-value=".m{{$a->id}}" href="#">{{$a->name}}</a></li>
+            @endforeach
+          </ul>
+        </div>
 
 
-        <select class="filters-select">
-          <option value="">Audiences</option>
-          @foreach($audiences as $a)
-          <option value=".a{{ $a->id }}">{{$a->name}}</option>
-          @endforeach
-        </select>
+        <div class="btn-group">
+          <button id="dropdown-thematic" type="button" class="btn btn-urc-secondary dropdown-toggle filters-dropdown" data-value="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            All Thematic Areas <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="do_filter" data-filter="thematic" data-name="All Thematic Areas" data-value="" href="#">All Thematic Areas</a></li>
+            @foreach($thematics as $a)
+              <li><a class="do_filter" data-filter="thematic" data-name="{{$a->name}}" data-value=".t{{$a->id}}"  href="#">{{$a->name}}</a></li>
+            @endforeach
+          </ul>
+        </div>
 
-        <button type="button" class="btn btn-urc-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Countries <span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu">
-          <li><a href="#">Bangladesh</a></li>
-          <li><a href="#">Paraguay</a></li>
-          <li><a href="#">Tanzania</a></li>
-          <li><a href="#">Burundi</a></li>
-        </ul>
-      </div>
 
-      <div class="btn-group">
-        <button type="button" class="btn btn-urc-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Methods <span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu">
-          @foreach($methods as $m)
-            <li><a href="#">{{$m->name}}</a></li>
-          @endforeach
-        </ul>
-      </div>
-    </ul>
+        <div class="btn-group">
+          <button id="dropdown-audience" type="button" class="btn btn-urc-secondary dropdown-toggle filters-dropdown" data-value="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            All Audiences <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="do_filter" data-filter="audience" data-name="All Audiences" data-value="" href="#">All Audiences</a></li>
+            @foreach($audiences as $a)
+              <li><a class="do_filter" data-filter="audience" data-name="{{$a->name}}" data-value=".a{{$a->id}}"  href="#">{{$a->name}}</a></li>
+            @endforeach
+          </ul>
+        </div>
+
+
+
+
+      </ul>
     </div>
   </nav>
 
--- resources/views/scripts/masonry.blade.php
+++ resources/views/scripts/masonry.blade.php
@@ -3,6 +3,37 @@
 <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
 <script type="text/javascript">
 
+
+  function call_filter(filter, value){
+    console.log('filter: '+filter+' :: '+value);
+  }
+
+  $('.do_filter').click( function(event){
+    filter= $(this).attr('data-filter');
+    name= $(this).attr('data-name');
+    value= $(this).attr('data-value');
+
+    dropdown= '#dropdown-'+filter;
+
+    $(dropdown).html( name+' <span class="caret"></span>');
+    $(dropdown).attr('data-value', value);
+
+
+    console.log(' '+filter+' '+value);
+
+    // recreate the whole filter string  ex: '.country.method.audience'
+    // this will match casestudies that have ANY of these filters
+    filters='';
+    $('.filters-dropdown').each(function(index) {
+      filters += $(this).attr('data-value');
+    });
+
+    console.log('filters: '+filters);
+
+    $grid.isotope({ filter: filters });  //apply the new filter to the grid
+
+  });
+
   /* initialize the masonry grid */
   var $grid= $('.grid').isotope({
     itemSelecter: '.grid-item',
@@ -28,7 +59,7 @@
     });
 
     $grid.isotope({ filter: filters });  //apply the new filter to the grid
-
+    console.log('filters: '+filters);
   });
 
 </script>
